285d17cc760f795a0e0b33885e394c03cea9ee26
ikram <ikrammarbasy17@gmail.com>
2025-03-02 16:55:31 +0700

    Add pasring data from URL documentation

--- templating2.py
+++ templating2.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app3 = Flask(__name__)
 
@@ -21,6 +21,29 @@ def contact():
 @app3.route("/about")
 def about():
     return render_template("about.html")
+
+#Parsing nilai int
+#Mengambil langsung data dari URL dan menggunakan nya di dalam aplikasi kita
+@app3.route("/parsing/<int:nilai1>")#Jika URL yang di input bukan bertipe int dia akan error 404
+def parsing1(nilai1):
+    #Bagian {} adalah placeholder dalam string
+    #.format() akan menggantikan {} dengan nilai variable (nilai1)
+    #Intinya digunakan untuk memasukan nilai variable ke dalam teks string
+    return "Nilainya adalah : {}".format(nilai1)
+
+#Parsing nilai String
+@app3.route("/parsing2/<string:nama>")
+def parsing2(nama):
+    return "Nama saya adalah : {}".format(nama)
+
+#Argument Parser
+#Menggunakan query parameter
+#Mengambil data dari parameter query setelah tanda ? di URL
+@app3.route("/parsing3")
+def parsing3():
+    data = request.args.get("nilai2")#Mengambil nilai query (nilai2)
+    return "Nilainya adalah : {}".format(data)
+
 
 if __name__ == "__main__":
     app3.run(debug=True)
